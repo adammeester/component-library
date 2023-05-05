b682c128df63799e94ced5d32ac6cea5337db63a
adammeester <48937876+adammeester@users.noreply.github.com>
2023-05-05 21:02:45 +1000

    test2

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,10 @@
 import json from '@rollup/plugin-json';
 import { vanillaExtractPlugin } from '@vanilla-extract/rollup-plugin';
-import resolve from '@rollup/plugin-node-resolve';
-// import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import commonjs from '@rollup/plugin-commonjs';
 import path from 'path';
-import postcss from 'rollup-plugin-postcss';
 import dts from 'rollup-plugin-dts';
 import esbuild from 'rollup-plugin-esbuild';
 import depsExternal from 'rollup-plugin-node-externals';
 import ts from 'typescript';
-import typescript from '@rollup/plugin-typescript';
 
 const loadCompilerOptions = (tsconfig) => {
   if (!tsconfig) return {};
@@ -22,43 +17,9 @@ const loadCompilerOptions = (tsconfig) => {
   return options;
 };
 
-const globals = {
-  react: 'React',
-  'react-dom': 'ReactDOM',
-  'core-js': 'core-js',
-  '@vanilla-extract/css': '@vanilla-extract/css',
-};
-
-const globalModules = Object.keys(globals);
-
 const compilerOptions = loadCompilerOptions('tsconfig.json');
 
-const additionalConfig = {
-  external: (id) => globalModules.includes(id) || /core-js/.test(id),
-};
-
-const additionalOutputsConfig = {
-  globals,
-  generatedCode: {
-    constBindings: true,
-  },
-  treeshake: false,
-};
-const resolveExtensions = ['.js', '.jsx', '.ts', '.tsx'];
-
-const plugins = [
-  depsExternal(),
-  // vanillaExtractPlugin(),
-  typescript({
-    allowJs: true,
-    jsx: 'react',
-    tsconfig: './tsconfig.json',
-  }),
-  resolve({ extensions: resolveExtensions }),
-  postcss(),
-  esbuild(),
-  json(),
-];
+const plugins = [vanillaExtractPlugin(), depsExternal(), esbuild(), json()];
 
 export default [
   {
@@ -71,10 +32,10 @@ export default [
         preserveModules: true,
         preserveModulesRoot: 'src',
 
-        // // Change .css.js files to something else so that they don't get re-processed by consumer's setup
-        // entryFileNames({ name }) {
-        //   return `${name.replace(/\.css$/, '.css.vanilla')}.js`;
-        // },
+        // Change .css.js files to something else so that they don't get re-processed by consumer's setup
+        entryFileNames({ name }) {
+          return `${name.replace(/\.css$/, '.css.vanilla')}.js`;
+        },
 
         // Apply preserveModulesRoot to asset names
         assetFileNames({ name }) {
@@ -82,10 +43,8 @@ export default [
         },
 
         exports: 'named',
-        ...additionalOutputsConfig,
       },
     ],
-    ...additionalConfig,
   },
   // Declaration files
   {
